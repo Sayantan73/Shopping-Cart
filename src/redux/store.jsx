3e67a544b7b14a4c8f8a25ea6bcbdd8a291b0f3f
Sayantan Karan <112612792+Sayantan73@users.noreply.github.com>
2024-11-14 23:52:11 +0530

    Update store.jsx
    Update import statement

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import  CounterSlice  from "./slices/CounterSlice";
-import  CartSlice  from "./slices/cartSlice";
+import  CartSlice  from "./slices/CartSlice";
 
 
 export const store = configureStore({
